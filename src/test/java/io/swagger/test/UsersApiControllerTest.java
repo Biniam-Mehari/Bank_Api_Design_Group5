17836119ea3bc46d6453dba65f7e3dcebeb1ea7d
Abhi6921! <abhishek.narvekar80@gmail.com>
2022-06-15 12:18:00 +0200

    changes

--- src/test/java/io/swagger/test/UsersApiControllerTest.java
+++ src/test/java/io/swagger/test/UsersApiControllerTest.java
@@ -1,5 +1,7 @@
 package io.swagger.test;
 
+import io.swagger.model.Account;
+import io.swagger.model.AccountType;
 import io.swagger.model.Role;
 import io.swagger.model.User;
 import io.swagger.repository.UserRepository;
@@ -7,16 +9,68 @@ import io.swagger.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ContextConfiguration("/application-context.xml")
 class UsersApiControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+    private User user;
+    private User admin;
+    private Account currentAccount;
+    private Account savingAccount;
+
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    public void setup() {
+        user = new User();
+        user.setUserId(1);
+        user.setFullname("Patrick Jane");
+        user.setUsername("pat");
+        user.setRoles(Arrays.asList(Role.ROLE_USER));
+        user.setPassword(passwordEncoder.encode("pat123"));
+
+        admin = new User();
+        admin.setUserId(2);
+        admin.setFullname("Thresa Lisbon");
+        admin.setUsername("thr");
+        admin.setRoles(Arrays.asList(Role.ROLE_ADMIN));
+        admin.setPassword("lis123");
+
+        currentAccount = new Account("NL31INHO0126400051", user, 20000.00, AccountType.current);
+        savingAccount = new Account("NL41INHO0226400031", user, 1300.00, AccountType.saving);
+
+    }
+
+    @Test
+    @WithMockUser(username = "thr", password = "lis123", roles = "ADMIN")
+    public void callingAllUsersWithAdminRightsShouldReturnOK() throws Exception {
+        given(userService.getAllUsers(0, 10, 0)).willReturn(Arrays.asList(user));
+        mockMvc.perform(get("/bankAPI/users")).andExpect(status().isOk());
+    }
+
+    /*
     @Autowired
     private UserService userService;
 
@@ -57,4 +111,6 @@ class UsersApiControllerTest {
         User user = userService.createUser("pat", "Patrick Jane", "pat123", 0);
         assertEquals(user.getUsername(), testuser1.getUsername());
     }
+    */
+
 }
